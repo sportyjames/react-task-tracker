c891fd8f25e8f1daad193a1f1e50d3175a8c060e
sportyjames <60296961+sportyjames@users.noreply.github.com>
2021-06-24 14:04:00 -0500

    control the user's typing speed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,10 +5,41 @@ const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
+    
+    const [isTimeoutRunning, setIsTimeoutRunning] = useState(false)
+
+    const onInputChange = (e, className) => {
+        if(isTimeoutRunning){    //third
+            e.preventDefault()
+            e.stopPropagation()
+        }
+        else
+        {
+            if(className == '1')
+            {
+                setText(e.target.value)   //first
+            }
+            else
+            {
+                setDay(e.target.value)   //first
+            }
+           
+            setIsTimeoutRunning(true) //second
+
+            setTimeout(() => {
+                setIsTimeoutRunning(false)  //fourth
+            },200) //suspend 0.2 second
+        }
+    }
+
+
+
+
 
     const onSubmit = (e) => {
         e.preventDefault(); //prevent refresh the page 
-        
+        //do the form validation here 
+
         if(!text)
         {
             alert('Please add a task')
@@ -20,18 +51,23 @@ const AddTask = ({ onAdd }) => {
         setText('')
         setDay('')
         setReminder(false)
-
     }
+
+
 
     return (
         <form className='add-form' onSubmit={onSubmit} >
             <div className='form-control'>
                 <label>Task</label>
-                <input type="text" placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)} />
+                <input className="1" type="text" placeholder='Add Task' value={text} onChange={(e) => {
+                    onInputChange(e, '1')
+                }} />
             </div>
             <div className='form-control'>
                 <label>Day & Time</label>
-                <input type="text" placeholder='Add Day & Time' value={day} onChange={(e) => setDay(e.target.value)} />
+                <input className="2" type="text" placeholder='Add Day & Time' value={day} onChange={(e) => {
+                    onInputChange(e, '2')
+                }} />
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
